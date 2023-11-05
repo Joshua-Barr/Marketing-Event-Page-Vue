f7e28660f4c10db771be3024f46d0c6540ba014f
Joshua-Barr <joshbarrscience@gmail.com>
2023-11-05 21:05:41 +0800

    Commit from TeleportHQ

---
+++ src/components/navigation-links.vue
@@ -0,0 +1,131 @@
+<template>
+  <nav class="navigation-links-nav" v-bind:class="rootClassName">
+    <span class="navigation-links-text Navigation-Link">{{ link1 }}</span>
+    <span class="navigation-links-text1 Navigation-Link">{{ link2 }}</span>
+    <span class="navigation-links-text2 Navigation-Link">{{ link3 }}</span>
+    <span class="navigation-links-text3 Navigation-Link">{{ link4 }}</span>
+    <span class="navigation-links-text4 Navigation-Link">{{ link5 }}</span>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavigationLinks',
+  props: {
+    rootClassName: String,
+    link2: {
+      type: String,
+      default: 'Services',
+    },
+    link3: {
+      type: String,
+      default: 'Resources',
+    },
+    link1: {
+      type: String,
+      default: 'Home',
+    },
+    link4: {
+      type: String,
+      default: 'Contact',
+    },
+    link5: {
+      type: String,
+      default: 'Client Login',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.navigation-links-nav {
+  flex: 0 0 auto;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+}
+.navigation-links-text {
+  color: var(--dl-color-scheme-white);
+  cursor: pointer;
+  transition: 0.3s;
+}
+.navigation-links-text:hover {
+  color: var(--dl-color-scheme-pink);
+}
+.navigation-links-text1 {
+  color: var(--dl-color-scheme-white);
+  cursor: pointer;
+  transition: 0.3s;
+  margin-left: var(--dl-space-space-threeunits);
+}
+.navigation-links-text1:hover {
+  color: var(--dl-color-scheme-pink);
+}
+.navigation-links-text2 {
+  color: var(--dl-color-scheme-white);
+  cursor: pointer;
+  transition: 0.3s;
+  margin-left: var(--dl-space-space-threeunits);
+}
+.navigation-links-text2:hover {
+  color: var(--dl-color-scheme-pink);
+}
+.navigation-links-text3 {
+  color: var(--dl-color-scheme-white);
+  cursor: pointer;
+  transition: 0.3s;
+  margin-left: var(--dl-space-space-threeunits);
+}
+.navigation-links-text3:hover {
+  color: var(--dl-color-scheme-pink);
+}
+.navigation-links-text4 {
+  color: var(--dl-color-scheme-white);
+  cursor: pointer;
+  transition: 0.3s;
+  margin-left: var(--dl-space-space-threeunits);
+  padding-top: var(--dl-space-space-halfunit);
+  border-color: var(--dl-color-scheme-pink);
+  border-width: 5px;
+  padding-left: 4px;
+  border-radius: var(--dl-radius-radius-radius4);
+  padding-right: 4px;
+  padding-bottom: var(--dl-space-space-halfunit);
+}
+.navigation-links-text4:hover {
+  color: var(--dl-color-scheme-pink);
+}
+
+
+
+
+
+
+
+
+@media(max-width: 767px) {
+  .navigation-links-nav {
+    align-items: flex-start;
+    flex-direction: column;
+  }
+  .navigation-links-text {
+    margin-bottom: var(--dl-space-space-unit);
+  }
+  .navigation-links-text1 {
+    margin-left: 0;
+    margin-bottom: var(--dl-space-space-unit);
+  }
+  .navigation-links-text2 {
+    margin-left: 0;
+    margin-bottom: var(--dl-space-space-unit);
+  }
+  .navigation-links-text3 {
+    margin-left: 0;
+    margin-bottom: var(--dl-space-space-unit);
+  }
+  .navigation-links-text4 {
+    margin-left: 0;
+    margin-bottom: var(--dl-space-space-unit);
+  }
+}
+</style>
---
+++ src/components/slide.vue
@@ -0,0 +1,55 @@
+<template>
+  <div data-type="slide" class="slide-slide slide" v-bind:class="rootClassName">
+    <div class="slide-max-wdith max-content-container">
+      <h1 class="slide-text Heading2">{{ heading }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Slide',
+  props: {
+    heading: {
+      type: String,
+      default: 'Slide #1',
+    },
+    rootClassName: String,
+  },
+}
+</script>
+
+<style scoped>
+.slide-slide {
+  align-items: center;
+  justify-content: center;
+}
+.slide-max-wdith {
+  flex: 0 0 auto;
+  display: flex;
+  flex-direction: column;
+}
+.slide-text {
+  color: var(--dl-color-scheme-white);
+}
+.slide-root-class-name {
+  background-color: #ff9b1d;
+}
+.slide-root-class-name1 {
+  background-color: #15bd19;
+}
+.slide-root-class-name2 {
+  background-color: #FB275A;
+}
+.slide-root-class-name3 {
+  background-color: #0975F1;
+}
+.slide-root-class-name4 {
+  background-color: #0D1F3A;
+}
+@media(max-width: 991px) {
+  .slide-root-class-name4 {
+    background-color: #0D1F3A;
+  }
+}
+</style>
